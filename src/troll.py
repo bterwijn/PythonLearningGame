f02ca68156c9c0d9f1a8043cea61aa7c530af217
Bas Terwijn <bterwijn@gmail.com>
2026-08-01 14:09:44 +0200

    made the Troll smaller and a bit less aggressive

--- src/troll.py
+++ src/troll.py
@@ -11,8 +11,9 @@ class Troll(Unit):
     image = pygame.image.load("assets/troll.png")
 
     def __init__(self, *args, **kwargs):
+        width, height = Troll.image.get_size()
         super().__init__(*args, **kwargs,
-                         radius=125,
+                         radius=(width+height)/4,
                          attack=999999,
                          hitpoints=400)
         self.randomize_acceleration()
@@ -22,19 +23,20 @@ class Troll(Unit):
         Troll.count -= 1
 
     def randomize_acceleration(self):
-        self.acceleration = random.uniform(-1.0, 0.5)
+        self.acceleration = random.uniform(-0.2, 0.5)
 
     def step(self):
-        if random.randint(1, 60) == 1:
+        if random.randint(1, 10) == 1:
             self.randomize_acceleration()
         direction = globals.player.get_position() - self.get_position()
         direction.normalize_ip()
         self.speed += direction * self.acceleration
-        self.speed *= 0.98
+        self.speed *= 0.95
         super().step()
 
     def draw(self, surface):
         dst = self.position.copy()
-        dst.x -= 125
-        dst.y -= 125
+        width, height = Troll.image.get_size()
+        dst.x -= width / 2
+        dst.y -= height / 2
         surface.blit(Troll.image, dst)
