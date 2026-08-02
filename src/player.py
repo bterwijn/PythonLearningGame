0f25c24c786e47e5f5ca5f6330c1df171016709f
Bas Terwijn <bterwijn@gmail.com>
2026-08-02 20:57:18 +0200

    player gets a direction to move and shoot in

--- src/player.py
+++ src/player.py
@@ -9,19 +9,23 @@ class Player(Unit):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs, color=(200, 200, 200), radius=20, line_width=6)
+        width, height = globals.display.get_size()
+        middle = pygame.Vector2(width/2, height/2)
+        self.direction = (middle - self.position).normalize()
         self.token_count = 0
         self.last_shot_time = 0
 
     def handle_keys(self, keys):
-        delta_speed = 0.5
+        acceleration = 0.5
+        rotate_speed = 4  # degrees per frame
         if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-            self.speed.x -= delta_speed
+            self.direction.rotate_ip(-rotate_speed)
         if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-            self.speed.x += delta_speed
+            self.direction.rotate_ip(rotate_speed)
         if keys[pygame.K_UP] or keys[pygame.K_w]:
-            self.speed.y -= delta_speed
+            self.speed += self.direction * acceleration
         if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-            self.speed.y += delta_speed
+            self.speed -= (self.direction/2) * acceleration
         if keys[pygame.K_SPACE] or keys[pygame.K_RETURN]:
             self.shoot()
 
@@ -30,15 +34,19 @@ class Player(Unit):
         if current_time - self.last_shot_time < 100:  # ms between shots
             return  # too soon to shoot again
         bullet_speed = 12
-        direction = pygame.Vector2(1, 0)  # shoot in X-direction
-        bullet_position = self.get_position() + direction * (self.radius + 10)  # spawn bullet just outside the player
-        bullet_speed = direction.elementwise() * self.speed + direction * bullet_speed
+        bullet_position = self.get_position() + self.direction * (self.radius * 1.5)  # spawn bullet just outside the player
+        bullet_speed = self.direction * bullet_speed
         globals.units.append(Bullet(position=bullet_position, speed=bullet_speed))
         self.last_shot_time = pygame.time.get_ticks()  # update last shot time
 
     def step(self):
         super().step()      # normal step behavior super class Unit
         self.speed *= 0.94  # apply friction to slow down the player over time
+
+    def draw(self, surface):
+        super().draw(surface)  # normal draw behavior super class Unit
+        end_pos = self.position + self.direction * (self.radius * 1.5)
+        pygame.draw.line(surface, (255, 255, 255), self.position, end_pos, self.line_width)
 
     def collision(self, other):
         if isinstance(other, Token):  # special collision behavior for Token
