a0069d1bcaf681759b2de56b7837df15109819fc
Anonymous Me <anonymous_me_me_me@proton.me>
2026-08-15 17:33:36 +0200

    Putler units added, any likeness to real people is completely coincidental

--- src/game.py
+++ src/game.py
@@ -10,6 +10,7 @@ from src.troll import Troll
 from src.bullet import Bullet
 from src.explosion import Explosion
 from src.tail import Tail
+from src.putler import Putler
 from src import utils
 
 class Game:
@@ -33,7 +34,7 @@ class Game:
             globals.units.append(unit)
 
     def spawn_units(self):
-        spawn_types = [Token, Hazard, Seeker, Troll]
+        spawn_types = [Token, Hazard, Seeker, Troll, Putler]
         for spawn_type in spawn_types:
             if spawn_type.count < spawn_type.max_count:
                 if random.random() < spawn_type.spawn_chance:
